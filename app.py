939f738a35d3412c9d156f63a7ffc078072abb82
Devansu <devansuyadav@gmail.com>
2021-03-19 12:22:38 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail,Message
+import json
+from datetime import datetime
+@app.route("/", methods=['GET', 'POST'])
+def library():
+    print("Helo")
+    if request.method == 'POST':
+        print("Helo")
+        '''Add entry to the database'''
+
+    return render_template('index.html')
